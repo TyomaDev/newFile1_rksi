73b5383cb110c0e0f1755c2ee485e7dc0764b494
Harry <57110073+MRXTOOR@users.noreply.github.com>
2022-12-17 23:26:19 +0300

    32132

--- php/view/pages/include/register.php
+++ php/view/pages/include/register.php
@@ -17,28 +17,27 @@ $otdel = $_POST['otdel'];
 
 if ($password === $password_confirm) {
     $path = 'uploads/' . time() . $_FILES['avatar']['name'];
-        if (!move_uploaded_file($_FILES['avatar']['tmp_name'], '../' . $path)) {
+        if (!move_uploaded_file($_FILES['avatar']['name'], '../' . $path)) {
             $_SESSION['message'] = 'Ошибка при загрузке сообщения';
             header('Location: ../../../../index.php');
             $password = md5($password);
-            mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-            header('Location: ../../../../index.php');
-        mysqli_query($connect, "INSERT INTO `users` (`id`, `full_name`, `login`, `email`, `password`, `avatar`) VALUES (NULL, '$full_name', '$login', '$email', '$password', '$path')");
-       // mysqli_query($connect, "INSERT INTO `users` (`id`, `full_name`, `login`, `email`, `password`, `avatar`) VALUES (NULL, '2@dsas', '2@dsas', '2@dsas', '2@dsas', 'NULL')");
-
-       mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-        $_SESSION['message'] = 'Регистрация прошла успешно!';
-        header('Location: ../../../../index.php');
-        echo $_SESSION['message'];
+           
         }
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-        header('Location: ../../../../index.php');
+        
+            
+        mysqli_query($connect, "INSERT INTO `users` (`id`, `full_name`, `login`, `email`, `password`, `avatar`) VALUES (NULL, '$full_name', '$login', '$email', '$password', '../uploads/')");
+       // mysqli_query($connect, "INSERT INTO `users` (`id`, `full_name`, `login`, `email`, `password`, `avatar`) VALUES (NULL, '2@dsas', '2@dsas', '2@dsas', '2@dsas', 'NULL')");
+
+        $_SESSION['message'] = 'Регистрация прошла успешно!';
+        
+        echo $_SESSION['message'];
 
 } else{
     $_SESSION['message1'] = 'Пароли не сходятся';
     header('location: ../../../../index.php');;
 }
-header('Location: ../../../../index.php'); 
+
 ?>
 
 
