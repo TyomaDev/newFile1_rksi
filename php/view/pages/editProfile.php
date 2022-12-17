c87b3e037911653d99722e2226a61a9d69f8493c
Artyom <artyom.musharov@mail.ru>
2022-12-17 14:53:45 +0300

    edit profile musharov 1
    edit profile complite

--- php/view/pages/editProfile.php
+++ php/view/pages/editProfile.php
@@ -1,38 +1,57 @@
 
 <!-- левые карточки -->
 <div class="add_catalog_cards">
-    <div class="upper_cards">
+    <div class="upper_cards shadow_def">
         <h4>Ваши каталоги</h4>
         <h5>Здесь показаны ваши каталоги, которые вы добавили</h5>
         <a>Открыть всё</a>
     </div>
 
     <!-- массив, перебираются каталоги -->
-    <div class="catalog_card">
-        <img src="" alt="download">
-        <h4>name catalog</h4>
-        <h6>Описание и дилинный текст Описание и дилинный текстнный  и дилинный текстн...  </h6>
-        <a>Посмотреть</a>
+   <div class="catalog_cards">
+        <div class="catalog_card shadow_def">
+            <!-- <img src="" alt="download"> -->
+            <h4>name catalog</h4>
+            <h6>Описание и дилинный текст Описание и дилинный и текстн...  </h6>
+            <a>Посмотреть</a>
+        </div>
+        <div class="catalog_card shadow_def">
+            <!-- <img src="" alt="download"> -->
+            <h4>name catalog</h4>
+            <h6>Описание и дилинный текст Описание и дилинный и текстн...  </h6>
+            <a>Посмотреть</a>
+        </div>
+        
     </div>
-    <!-- массив -->
-
     <div class="pagination">
-        <span><</span>
+        <span>←</span>
         <span>1</span>
         <span>2</span>
-        <span>></span>
-    </div>
+        <span>→</span>
+   </div>
+    <!-- массив -->
+
+    
 </div>
 
 <!-- фото и поля для редактирования -->
 
-<div class="edit_profile_caards">
-    <img src="images/rkt/jpg" alt="">
+<div class="edit_profile_cards">
+    
     <form>
-    <input type="text" name="FIO" value="Фамилия Имя Отчетво" />
-    <input type="data" name="data" value="20.12.2002" />
-    <input name="data" value="Участник"/>
-    <input type="email" name="email" value="Фамилия Имя Отчетво"/>
-    <input  type="password"  name="password" value="passwordpassword" />
+        <div class="top-part_edit_profile">
+            <img class="editProf_img" src="images/rkt.jpg" alt="">
+            <div>
+                <input class="editP_input shadow_def" name="data" value="Участник"/>
+                <input class="editP_input shadow_def" type="text" name="FIO" value="Фамилия Имя Отчетво" />
+                <input class="editP_input shadow_def" type="data" name="data" value="20.12.2002" />
+            </div>
+        </div>
+    <div class="epc_bottom_inputs"> 
+        <input class="editP_input shadow_def" type="email" name="email" value="adasdasd@mail.ru"/>
+        <input  class="editP_input shadow_def" type="password"  name="password" value="passwordpassword" />
+        <button class="editP_input shadow_def editP_input_submit" type="submit">save</button>
+    </div>
     </form>
+
 </div>
