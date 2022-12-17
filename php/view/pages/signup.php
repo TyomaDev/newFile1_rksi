d67e3897382e89efced0b868960b80a9f9ddddfd
sijka <120601244+sijka@users.noreply.github.com>
2022-12-17 18:01:04 +0300

    tresh

--- php/view/pages/signup.php
+++ php/view/pages/signup.php
@@ -1,13 +1,24 @@
 <section>
 <form class="signup_form" action="POST">
-        <h3>Регистрация</h3>
-        <input type="text" name="login" placeholder="login">
-        <input type="text" name="FIO" placeholder="FIO">
-        <input type="text" name="email" placeholder="email">
-        <hr>
+        <h3 class="form-title">Регистрация</h3>
+        <input type="text" name="full_name" placeholder="Введите полное имя">
+        <input type="email" name="mail" placeholder="email">
+<input type="date" name="dates" placeholder="data 31.12.2022">  
+        <select name="otdel" id="pet-select">
+    <option value="">название отделов
+    </option>
+    <option value="kom">комерческий</option>
+    <option value="fin">финансовый</option>
+    <option value="market">маркетинговый</option>
+        
+</select>   
         <input type="password" name="password" placeholder="password">
         <input type="password" name="password_confirm" placeholder="confirm password">
-        <button type="submit">Подтвердить</button>
+        <label class="input-file">
+        <input type="file" name="avatar">
+        <span>Выберите файл</span>
+        </label>
+        <button type="submit">Зарегестрироваться</button>
 
     </form>
 </section>
